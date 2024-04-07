a2151d250e1db2964f8736ed31db6bef92ed471e
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2024-04-07 16:16:47 +0200

    remove blogs

--- presentation.js
+++ presentation.js
@@ -1,3 +1,41 @@
-function back() {
-    window.location.pathname = "index.html";
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("mySlides");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+showSlides(index);
+
+function plusSlides(n) {
+  showSlides(index += n);
+}
+
+function currentSlide(n) {
+  showSlides(index = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {index = 1}    
+  if (n < 1) {index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[index-1].style.display = "block";  
+}
+function togglenavbar() {
+   var navbar = document.querySelector(".navbar-sm");
+   navbar.classList.toggle("show");
 }
