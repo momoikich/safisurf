85b577a3ab1367e899e6450096d0e27f699f371b
momoikich <103676649+momoikich@users.noreply.github.com>
2024-04-09 18:30:00 +0200

    add media navbar

--- media.js
+++ media.js
@@ -2,3 +2,12 @@ function togglenavbar() {
    var navbar = document.querySelector(".navbar-sm");
    navbar.classList.toggle("show");
 }
+
+
+let btnNavbars = document.querySelectorAll('.u-section-navbar a')
+btnNavbars.forEach((btnNavbar)=>{
+    btnNavbar.addEventListener('click',(e)=>{
+        btnNavbars.forEach(el=> el.classList.remove('navbar-active'))
+        e.target.classList.toggle('navbar-active')
+    })
+})
